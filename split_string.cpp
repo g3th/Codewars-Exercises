156bc5e58e1046c8c5adc9bf914eb80b707d7572
g3th <theinkandpen@yahoo.co.uk>
2022-11-11 01:18:28 +0000

    exercise 1 - split strings 	modified:   split_string.cpp

--- split_string.cpp
+++ split_string.cpp
@@ -3,27 +3,26 @@
 #include <vector>
 using namespace std;
 
-/*Codewars beginner assignment*/
-
-vector<string> test(string original_string){
-	
-	vector<string> split_string;
-	if (original_string.size() % 2 ==0){
-		cout << "String is even." << endl;
-	} else {
-		original_string += "_";
-	}
-	for (int x=0; x < original_string.size(); ++++x){
-		split_string.push_back(original_string.substr(x, 2));
-	}
-	return split_string; 
+vector<string> solution(const string &s)
+{   string add_to;
+    add_to.assign(s);
+    vector<string> split_string;
+    if (s.size() % 2 ==0){
+      cout << "String is even." << endl;
+    } else {
+       add_to += "_";
+      }
+    for (int x=0; x < add_to.size(); ++++x){
+      split_string.push_back(add_to.substr(x, 2));
+    }
+    return split_string;
 }
 
 int main(){
 	string user_input = "";
 	cout << "Enter your string (No spaces pls): ";
 	cin >> user_input;
-	vector<string> split_string = test(user_input);
+	vector<string> split_string = solution(user_input);
 	for (int x = 0; x < split_string.size(); ++x){
 		cout << split_string[x] << endl;
 	}
